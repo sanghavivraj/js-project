865cbb8b4bf597b9704b94a70cf6463b9d6632ea
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-15 10:40:04 +0000

    conversation of datatypes

---
+++ basic_01/conversationOperation.js
@@ -0,0 +1,17 @@
+let score=null
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abs" =>NaN
+//true => 1
+//false =>0;
+
+let isLoggenIn=1;
+let boolenIsLoggedIn=Boolean(isLoggenIn)
+console.log(boolenIsLoggedIn);
+
+// there are also string in which we can also use the type of to convert the any datatype to string.
