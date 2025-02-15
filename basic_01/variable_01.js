c05dc14f5ea2a11adfd81f28428a2469e90c5e2e
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-15 10:29:10 +0000

    variable

---
+++ basic_01/variable_01.js
@@ -0,0 +1,10 @@
+const accountId="124558"
+let accountEmail="vraj@gmail.com"
+ accountCity="bhavnagar"
+ let accountState
+
+//  accountId="2354"
+ console.log(accountId);
+//  console.log(accountEmail);
+ console.table([accountId,,accountEmail,accountCity,accountState])
+ 
