e50c0d1996f13800b0772cbba6b087f8f8e83ff0
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-15 12:11:57 +0000

    datatype summary

---
+++ basic_01/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive 
+// 7 types: String ,number,bigInt,symbol, Boolean, null, undefined
+
+// const id=Symbol(123)
+
+// const anotherId=Symbol(123)
+// console.log(id==anotherId)
+
+// javascript is dynamic language
+
+// reference (Non primitive)
+
+// Array, Objects, Function
+
+
+ const heros=["vraj","shaktiman"]
+
+ console.log(heros);
+ 
+let obj={
+    name:"vraj",
+    dob:"01/02/2004",
+}
+console.log(obj)
+
+const myfunction=Function()
+{
+    console.log("hello vraj");
+    
+}
+console.log(myfunction);
+
+
