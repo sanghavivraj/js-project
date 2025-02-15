1285968afdc6e1e981fe7d81271216f06682885b
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-15 10:28:38 +0000

    datatypes

---
+++ basic_01/datatypes_02.js
@@ -0,0 +1,22 @@
+"use strict";//trat all the code as new js version
+//alert(3+3)//we are using nodejs not browser
+
+console.log(3+3);
+
+let number=5641
+let state=null
+//number => 2to power 53
+//bigit
+//string =>""
+//boolean => true/false
+//null=?standalone value
+//undefined => va;ue was not define
+//symbol=> for unique componet
+
+
+//object
+
+console.log(typeof "vraj");
+
+
+console.table([number,state]);
