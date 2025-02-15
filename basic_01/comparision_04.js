d7cb0df10153f644f0d5f3d3135cdad400887682
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-15 11:29:41 +0000

    comparision

---
+++ basic_01/comparision_04.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2!=1);
+// console.log(2>=1);
+//tjis is normal comparison which have answer in true or false
+
+
+// console.log("2">1); //in which we are tryinh to compare the string to number;
+// console.log("02">1)
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);   
+// and one more thing we can avoide this type of converation
+
+
