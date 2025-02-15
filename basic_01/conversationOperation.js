87b44281993aa61f6f6a8c6fd14c8f6fb4771b5f
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-15 11:06:49 +0000

    operation

--- basic_01/conversationOperation.js
+++ basic_01/conversationOperation.js
@@ -1,17 +1,45 @@
-let score=null
-console.log(typeof score)
+// let score=null
+// console.log(typeof score)
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
-//"33"=>33
-//"33abs" =>NaN
-//true => 1
-//false =>0;
+// //"33"=>33
+// //"33abs" =>NaN
+// //true => 1
+// //false =>0;
 
-let isLoggenIn=1;
-let boolenIsLoggedIn=Boolean(isLoggenIn)
-console.log(boolenIsLoggedIn);
+// let isLoggenIn=1;
+// let boolenIsLoggedIn=Boolean(isLoggenIn)
+// console.log(boolenIsLoggedIn);
 
-// there are also string in which we can also use the type of to convert the any datatype to string.
+// // there are also string in which we can also use the type of to convert the any datatype to string.
+
+
+
+
+//---------------------operation--------------------------
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+
+// let str1="hello"
+// let str2=" vraj"
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); //there are some rules of converstion
+// console.log(1+2+"2");
+
+console.log(true);
+
+
+
