4069b33fd1df1acb8727b466a5532f764ad9b857
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-16 14:59:06 +0000

    dates

---
+++ basic_01/dates_07.js
@@ -0,0 +1,27 @@
+//Dates
+let mydate=new Date()
+// console.log(mydate)
+
+// console.log(mydate.toString())
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof mydate)
+
+let mycreatedate=new Date(2025,0,5)
+// console.log(mycreatedate.toString())
+
+// console.log(mycreatedate.getTime());
+
+let newDate=new Date()
+console.log(newDate.getDay())
+console.log(newDate.getDay());
+console.log(newDate.getMonth()+1);
+
+
+
+
+
+
+
+
