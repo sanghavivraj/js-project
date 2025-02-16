ddadf850ef08913ae2a3e41ad6b3d79f0d49b674
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-16 14:11:38 +0000

    num_and_maths

---
+++ basic_01/nums_and_maths_06.js
@@ -0,0 +1,34 @@
+const score=340     
+// console.log(score)
+
+const blance=new Number(750)
+// console.log(blance)
+
+ 
+
+// console.log(blance.toString().length);
+
+const othernumber=123.89
+// console.log(othernumber.toPrecision(4))
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//++++++++++++++++++++++Maths--------------
+
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.54))
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+10)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
