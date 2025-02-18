bae65c2567394895a0607a09f8f0be5c1ce3168b
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-18 06:07:41 +0000

    02_arrays

---
+++ basic_02/02_arrays.js
@@ -0,0 +1,32 @@
+const marvel_heros=['spiderman','ironman','blackpanter']
+const dc_heros=['batman','jocker','flash']
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+// const allHeros=marvel_heros.concat(dc_heros)// it is used to add to array in new array
+// console.log(allHeros)
+
+// const new_allHero=[...marvel_heros,...dc_heros] // it also work same as concat but it's call spread which is used for 2 or more arrays also.
+// console.log(new_allHero)
+
+// const array_new=[1,2,[3,4,[5,6,7,],8],9]
+// const newa=array_new.flat(Infinity)
+// console.log(newa)
+
+
+
+
+
+// console.log(Array.from("vraj"))
+console.log(Array.from({name:"vraj"})) //iterseting  it give the empty array
+
+score1=100
+score2=200
+score3=300
+
+console.log(Array.of(score1,score2,score3)) //of is ude to convert the variable arrays in array
+
+
+
+
