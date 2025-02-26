85dcb098df0d5777b636c6055ee265e75b38af97
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-26 18:02:09 +0000

    04_iife

---
+++ basic_03/04_iife.js
@@ -0,0 +1,19 @@
+//Immediately invoked function expression(IIFE)
+
+(function chai()
+{
+    console.log(`DB Connected`)
+    
+})()
+
+
+
+
+// javascript execution context
+
+// when we create the javascript code first of all the globel EC is create and it is definetely done.
+
+//javascript is a single thread
+
+//-> golbal execution context
+//->Function execution context
