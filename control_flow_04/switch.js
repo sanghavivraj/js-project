98e6f193c25f310b905c92404463d2ce8c08e6c1
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-02-27 12:32:41 +0000

    switch.js

---
+++ control_flow_04/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }    it is the basic symtax of thhe switch case which we used when we can check che condtion multiple time with multiple values.
+
+const month=2
+
+switch (month) {
+    case 1:
+        console.log("junuary");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("enter valid number");
+        
+        break;
+}
